5f1921abf82915ba4fb52a42b0d6cde20708b9ed
Sergei Kudriavtsev <kudryavtsevsr@gmail.com>
2024-05-08 06:04:31 -0400

    init vite project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    {{ name }}
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const name = ref('Home')
+</script>
+
+<style scoped>
+  .home {
+    color: red;
+  }
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import path from 'path';
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "@/assets/styles/variables" as *;'
+      }
+    }
+  }
+})
